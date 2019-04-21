8b7b7bc504b473b7954b482d05c28c20c5452f4e
Mike Hatfield <oakraven13@gmail.com>
2019-04-21 18:54:27 -0300

    Added TodoItem to database.

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -8,24 +8,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
-    public class TodoController : Controller
+  public class TodoController : Controller
+  {
+    private readonly ITodoItemService _todoItemService;
+
+    public TodoController(ITodoItemService todoItemService)
     {
-        private readonly ITodoItemService _todoItemService;
-
-        public TodoController(ITodoItemService todoItemService)
-        {
-            _todoItemService = todoItemService;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var items = await _todoItemService.GetIncompleteItemsAsync();
-
-            var model = new TodoViewModel
-            {
-                Items = items
-            };
-
-            return View(model);
-        }
+      _todoItemService = todoItemService;
     }
+    public async Task<IActionResult> Index()
+    {
+      var items = await _todoItemService.GetIncompleteItemsAsync();
+
+      var model = new TodoViewModel
+      {
+        Items = items
+      };
+
+      return View(model);
+    }
+
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddItem(TodoItem newItem)
+    {
+      if (!ModelState.IsValid)
+      {
+        return RedirectToAction("Index");
+      }
+
+      var successful = await _todoItemService.AddItemAsync(newItem);
+      if (!successful)
+      {
+        return BadRequest("Could not add item.");
+      }
+
+      return RedirectToAction("Index");
+    }
+  }
 }
--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -5,8 +5,10 @@ using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Services
 {
-    public interface ITodoItemService
-    {
-        Task<TodoItem[]> GetIncompleteItemsAsync();
-    }
+  public interface ITodoItemService
+  {
+    Task<TodoItem[]> GetIncompleteItemsAsync();
+
+    Task<bool> AddItemAsync(TodoItem newItem);
+  }
 }
